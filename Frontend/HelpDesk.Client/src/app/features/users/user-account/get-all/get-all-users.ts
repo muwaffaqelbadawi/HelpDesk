959b2397f6fe7f059ba92b77169f2d051ad22531
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2026-09-04 23:50:34 +0300

    refactor(frontend): correct paths for create-user, delete-user, get-all-users, and get-by-id-user

---
+++ Frontend/HelpDesk.Client/src/app/features/users/user-account/get-all/get-all-users.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-users',
+  imports: [],
+  templateUrl: './get-all-users.html',
+  styleUrl: './get-all-users.css',
+})
+export class GetAllUsersComponent {}
