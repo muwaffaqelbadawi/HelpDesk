3b7a4318b05f006941a63de24a7884fae72bed15
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2026-09-11 09:13:41 +0300

    chore(Tests): configure integration testing and test dependencies
    - Set up the integration test project and configuration
    - Upgrade test packages
    - Remove the development connection string from appsettings.json

---
+++ Backend/HelpDesk.Tests.Integration/Features/Users/UserAccount/Create/CreateUserAccountTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace HelpDesk.Tests.Integration.Features.Users.UserAccount.Create;
+
+public sealed class CreateUserAccountTests
+{
+    [Fact]
+    public async Task Should_create_user_account()
+    {
+
+    }
+}
